70e30006b1222e175d6e982ce5f5628ac2bd3307
Joshua Archer <josharcher@me.com>
2017-11-28 23:06:11 -0800

    asdf

--- fall2016-midterm/index2.php
+++ fall2016-midterm/index2.php
@@ -4,8 +4,29 @@
 $dateOne = $_GET["date1"];
 $dateTwo = $_GET["date2"];
 
-print("date one: " + $dateOne);
-print("date two: " + $dateTwo);
+function writeContainer() {
+  print("date one: " + $dateOne);
+  print("date two: " + $dateTwo);
+
+  print("a;lsdkfa;sdlkfja;slkdjf");
+}
 
 
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+  <title>My First PHP Embedded Page</title>
+  <link rel="stylesheet" type="text/css" href="calendar.css" />
+</head>
+
+<body>
+  <div id="container">
+    <?php writeContainer(); ?>
+  </div>
+</body>
+
+</html>
