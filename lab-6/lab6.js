2118cf423aecf4a7c0532951e486db8f8b53db4c
Joshua Archer <josharcher@me.com>
2017-11-28 18:16:44 -0800

    lab 6 and hello_world

--- lab-6/lab6.js
+++ lab-6/lab6.js
@@ -1,4 +1,9 @@
 
+var max_move_count = 0;
+
+var amount_to_move = 400;
+var timeout_time = 5;
+
 function start() {
   console.log("startings");
   divObj = document.getElementById("box");
@@ -6,7 +11,8 @@ function start() {
   divObj.style.left = "10px";
   divObj.style.top = "10px";
 
-  move_it();
+  // move_it();
+  start_moving();
 }
 
 function move_it() {
@@ -14,5 +20,72 @@ function move_it() {
   var left_offset = divObj.style.left;
   left_offset = parseInt(left_offset) + 1;
   divObj.style.left = left_offset + "px";
-  setTimeout("move_it()", 20);
+  if (max_move_count < 200) {
+    max_move_count += 1;
+    setTimeout("move_it()", timeout_time);
+  }
+}
+
+function start_moving() {
+  move_right(amount_to_move);
+}
+
+var move_right_count = 0;
+function move_right(times) {
+  move_right_count += 1;
+  var left_offset = divObj.style.left;
+  left_offset = parseInt(left_offset) + 1;
+  divObj.style.left = left_offset + "px";
+  if (move_right_count < times) {
+    setTimeout(move_right, timeout_time, times);
+  } else {
+    console.log(move_right_count);
+    move_down(times);
+  }
+}
+
+var move_down_count = 0;
+function move_down(times) {
+  move_down_count += 1;
+  var top_offset = divObj.style.top;
+  top_offset = parseInt(top_offset) + 1;
+  divObj.style.top = top_offset + "px";
+  // var left_offset = divObj.style.left;
+  // left_offset = parseInt(left_offset) - 1;
+  // divObj.style.left = left_offset + "px";
+  if (move_down_count < times) {
+
+    setTimeout(move_down, timeout_time, times);
+  } else {
+    move_left(times);
+  }
+}
+
+var move_left_count = 0;
+function move_left(times) {
+  move_left_count += 1;
+  var left_offset = divObj.style.left;
+  left_offset = parseInt(left_offset) - 1;
+  divObj.style.left = left_offset + "px";
+  if (move_left_count < times) {
+    setTimeout(move_left, timeout_time, times);
+  } else {
+    move_up(times);
+  }
+}
+
+var move_up_count = 0;
+function move_up(times) {
+  move_up_count += 1;
+  var top_offset = divObj.style.top;
+  top_offset = parseInt(top_offset) - 1;
+  divObj.style.top = top_offset + "px";
+  // var left_offset = divObj.style.left;
+  // left_offset = parseInt(left_offset) - 1;
+  // divObj.style.left = left_offset + "px";
+  if (move_up_count < times) {
+    setTimeout(move_up, timeout_time, times);
+  } else {
+    console.log("DONEEEE");
+  }
 }
---
+++ lab-7/hello_world.php
@@ -0,0 +1,28 @@
+#!/usr/local/bin/php
+<?php
+
+
+// echo "Hello World";
+function writeContainer()
+{
+  echo "hello world";
+}
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+  <title>My First PHP Embedded Page</title>
+  <link rel="stylesheet" type="text/css" href="calendar.css" />
+</head>
+
+<body>
+  <div id="container">
+    <?php writeContainer(); ?>
+  </div>
+</body>
+
+</html>
