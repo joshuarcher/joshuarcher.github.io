66f6ff2bc261300a01b7c25a4cbb34a497deb9ba
Joshua Archer <josharcher@me.com>
2017-12-15 16:41:29 -0800

    asdf

--- final_project/js/fetch_posts.js
+++ final_project/js/fetch_posts.js
@@ -18,6 +18,63 @@ function fetchPosts() {
 
 function writePosts(postsJson) {
   var posts = postsJson["posts"];
+  for (i = 0; i < posts.length; i++) {
+
+    var listItem = document.createElement("div");
+    listItem.className = "listItem";
+
+    var listItemTop = document.createElement("div");
+    listItemTop.className = "listItemTop";
+    listItem.appendChild(listItem);
+
+    var rankSpan = document.createElement("span");
+    rankSpan.className = "rank";
+    rankSpan.innerHTML = `{i+1}.`; // JARCH
+    listItemTop.appendChild(rankSpan);
+
+    var upvoteSpan = document.createElement("span");
+    upvoteSpan.className = "upvote";
+    upvoteSpan.id = "up_1";
+    listItemTop.appendChild(upvoteSpan);
+
+    var upvoteLink = document.createElement("a");
+    upvoteLink.innerHTML = "<svg height=\"10\" width=\"10\"\><polygon points=\"10,10 5,0 0,10\" style=\"fill:#555;\" /></svg>";
+    upvoteSpan.appendChild(upvoteLink);
+
+    var titleSpan = document.createElement("span");
+    titleSpan.className = "title";
+    listItemTop.appendChild(titleSpan);
+
+    var titleLink = document.createElement("a");
+    titleLink.href = posts[0].url;
+    titleSpan.appendChild(titleLink);
+
+    document.getElementById("items").appendChild(listItem);
+
+    // `
+    // <div class="listItem">
+    //   <div class="listItemTop">
+    //     <span class="rank">1.</span>
+    //     <span id="up_123456" class="upvote">
+    //       <a href="#">
+    //         <svg height="10" width="10">
+    //           <polygon points="10,10 5,0 0,10" style="fill:#555;" />
+    //         </svg>
+    //       </a>
+    //     </span>
+    //     <span class="title">
+    //       <a href="#link">Overcoming Us vs. Them</a>
+    //     </span>
+    //   </div>
+    //   <div class="listItemBottom">
+    //     <span class="listLink">115 points</span> |
+    //     <span class="listLink">2 hours ago</span> |
+    //     <span class="listLink" onclick="hide">
+    //       <a href="#">hide</a>
+    //     </span>
+    //   </div>
+    // </div>`;
+  }
 
   var items = document.getElementById("items");
   items.innerHTML = posts;
