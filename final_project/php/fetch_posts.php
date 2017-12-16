6579982b9cc27931d52865eb9bbef60906aa67c9
Joshua Archer <josharcher@me.com>
2017-12-15 16:15:12 -0800

    work for me

--- final_project/php/fetch_posts.php
+++ final_project/php/fetch_posts.php
@@ -7,28 +7,36 @@ class Post {
   public $text;
   public $upvoteCount;
   public $timestamp;
+
+  public function __construct($title, $url, $text, $upvoteCount, $timestamp) {
+    $this->title = $title;
+    $this->url = $url;
+    $this->text = $text;
+    $this->upvoteCount = $upvoteCount;
+    $this->timestamp = $timestamp;
+  }
 }
 
-$postOne = Post();
-postOne->title = "This is a demo title";
-postOne->url = "http://google.com";
-postOne->text = "";
-postOne->upvoteCount = 27;
-postOne->timestamp = 1513381502687;
+$postOne = Post("This is a demo title 1", "http://google.com", "", 20, 1513381502687);
+// postOne->title = "This is a demo title";
+// postOne->url = "http://google.com";
+// postOne->text = "";
+// postOne->upvoteCount = 27;
+// postOne->timestamp = 1513381502687;
 
-$postTwo = Post();
-postTwo->title = "This is a demo title 2";
-postTwo->url = "http://google.com";
-postTwo->text = "";
-postTwo->upvoteCount = 27;
-postTwo->timestamp = 1513381502687;
+$postTwo = Post("This is a demo title 2", "http://google.com", "", 27, 1513381502687);
+// postTwo->title = "This is a demo title 2";
+// postTwo->url = "http://google.com";
+// postTwo->text = "";
+// postTwo->upvoteCount = 27;
+// postTwo->timestamp = 1513381502687;
 
-$postThree = Post();
-postThree->title = "This is a demo title 3";
-postThree->url = "http://google.com";
-postThree->text = "";
-postThree->upvoteCount = 27;
-postThree->timestamp = 1513381502687;
+$postThree = Post("This is a demo title 3", "http://google.com", "", 99, 1513381502687);
+// postThree->title = "This is a demo title 3";
+// postThree->url = "http://google.com";
+// postThree->text = "";
+// postThree->upvoteCount = 27;
+// postThree->timestamp = 1513381502687;
 
 $oneJson = json_encode($postOne);
 $twoJson = json_encode($postTwo);
