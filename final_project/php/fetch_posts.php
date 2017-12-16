a35a526c92575dae85c2327bed71430fafe62356
Joshua Archer <josharcher@me.com>
2017-12-15 16:17:13 -0800

    lessgo

--- final_project/php/fetch_posts.php
+++ final_project/php/fetch_posts.php
@@ -17,21 +17,21 @@ class Post {
   }
 }
 
-$postOne = Post("This is a demo title 1", "http://google.com", "", 20, 1513381502687);
+$postOne = new Post("This is a demo title 1", "http://google.com", "", 20, 1513381502687);
 // postOne->title = "This is a demo title";
 // postOne->url = "http://google.com";
 // postOne->text = "";
 // postOne->upvoteCount = 27;
 // postOne->timestamp = 1513381502687;
 
-$postTwo = Post("This is a demo title 2", "http://google.com", "", 27, 1513381502687);
+$postTwo = new Post("This is a demo title 2", "http://google.com", "", 27, 1513381502687);
 // postTwo->title = "This is a demo title 2";
 // postTwo->url = "http://google.com";
 // postTwo->text = "";
 // postTwo->upvoteCount = 27;
 // postTwo->timestamp = 1513381502687;
 
-$postThree = Post("This is a demo title 3", "http://google.com", "", 99, 1513381502687);
+$postThree = new Post("This is a demo title 3", "http://google.com", "", 99, 1513381502687);
 // postThree->title = "This is a demo title 3";
 // postThree->url = "http://google.com";
 // postThree->text = "";
