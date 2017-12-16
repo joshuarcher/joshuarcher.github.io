75ada2165bb4ba904b2f54f5072abff972d69728
Joshua Archer <josharcher@me.com>
2017-12-15 16:10:55 -0800

    asdf

--- final_project/js/fetch_posts.js
+++ final_project/js/fetch_posts.js
@@ -4,6 +4,7 @@ function fetchPosts() {
   console.log("fetching");
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
+      console.log(xhr.responseText);
       var posts = JSON.parse(xhr.responseText);
 
       writePosts(posts);
