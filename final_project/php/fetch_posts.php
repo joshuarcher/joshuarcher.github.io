5b4e81e1bd022b2f94d70d490ded343e6c4693f5
Joshua Archer <josharcher@me.com>
2017-12-15 16:04:12 -0800

    asdf

--- final_project/php/fetch_posts.php
+++ final_project/php/fetch_posts.php
@@ -1,40 +1,63 @@
 #!/usr/local/bin/php -d display_errors=STDOUT
 <?php
 
-class Post {
-  public $title;
-  public $url;
-  public $text;
-  public $upvoteCount;
-  public $timestamp;
-}
+echo '{
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}';
 
-$postOne = Post();
-postOne->title = "This is a demo title";
-postOne->url = "http://google.com";
-postOne->text = "";
-postOne->upvoteCount = 27;
-postOne->timestamp = 1513381502687;
-
-$postTwo = Post();
-postTwo->title = "This is a demo title 2";
-postTwo->url = "http://google.com";
-postTwo->text = "";
-postTwo->upvoteCount = 27;
-postTwo->timestamp = 1513381502687;
-
-$postThree = Post();
-postThree->title = "This is a demo title 3";
-postThree->url = "http://google.com";
-postThree->text = "";
-postThree->upvoteCount = 27;
-postThree->timestamp = 1513381502687;
-
-$oneJson = json_encode($postOne);
-$twoJson = json_encode($postTwo);
-$threeJson = json_encode($postThree);
-
-$arr = $arrayName = array('posts' => array($oneJson, $twoJson, $threeJson));
-
-echo json_encode($arr);
+// class Post {
+//   public $title;
+//   public $url;
+//   public $text;
+//   public $upvoteCount;
+//   public $timestamp;
+// }
+//
+// $postOne = Post();
+// postOne->title = "This is a demo title";
+// postOne->url = "http://google.com";
+// postOne->text = "";
+// postOne->upvoteCount = 27;
+// postOne->timestamp = 1513381502687;
+//
+// $postTwo = Post();
+// postTwo->title = "This is a demo title 2";
+// postTwo->url = "http://google.com";
+// postTwo->text = "";
+// postTwo->upvoteCount = 27;
+// postTwo->timestamp = 1513381502687;
+//
+// $postThree = Post();
+// postThree->title = "This is a demo title 3";
+// postThree->url = "http://google.com";
+// postThree->text = "";
+// postThree->upvoteCount = 27;
+// postThree->timestamp = 1513381502687;
+//
+// $oneJson = json_encode($postOne);
+// $twoJson = json_encode($postTwo);
+// $threeJson = json_encode($postThree);
+//
+// $arr = $arrayName = array('posts' => array($oneJson, $twoJson, $threeJson));
+//
+// echo json_encode($arr);
 ?>
